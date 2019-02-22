e0f2d2bb5c7c922a407bb8cafee3a7064c26341a
Stanislav Balia <balya.stanislav@gmail.com>
2019-02-22 22:33:20 +0200

    Refactoring unit tests for ScriptSources.

--- SQLWorker.BLL/ScriptSources.cs
+++ SQLWorker.BLL/ScriptSources.cs
@@ -6,12 +6,20 @@ namespace SQLWorker.BLL
 {
     public static class ScriptSources
     {
+        private static  object objLock = new object();
         private static readonly ConcurrentBag<ScriptInfo> Scripts = new ConcurrentBag<ScriptInfo>();
         public static IEnumerable<ScriptInfo> GetAll() => Scripts.ToList();
 
         public static void Add(ScriptInfo scriptInfo) => Scripts.Add(scriptInfo);
 
-        public static void RemoveAll() => Scripts.Clear();
+        public static void RemoveAll()
+        {
+            lock (objLock)
+            {
+                Scripts.Clear();    
+            }
+            
+        }
 
         public static void AddRange(List<ScriptInfo> list)
         {
--- SQLWorker.UnitTests/BLL/ScriptSourcesTests.cs
+++ SQLWorker.UnitTests/BLL/ScriptSourcesTests.cs
@@ -7,12 +7,15 @@ using Xunit;
 namespace SQLWorker.UnitTests.BLL
 {
     public class ScriptSourcesTests
-    { 
+    {
+        private readonly object _objLock = new object();
+        
         [Fact]
         public void AddSingleElement_ReturnsOneAsCountOfAll()
         {
-            ScriptSources.Add(new ScriptInfo());
-            ScriptSources.GetAll().Count().Should().Be(1);
+                ScriptSources.Add(new ScriptInfo());
+                ScriptSources.GetAll().Count().Should().Be(1);
+                ScriptSources.RemoveAll();
         }
         
         [Theory]
@@ -20,32 +23,31 @@ namespace SQLWorker.UnitTests.BLL
         [InlineData(3,3)]
         public void GetAll_ReturnsCurrentCount(int forI, int expectedCount)
         {
-            for(int i = 0; i < forI; i++)
-                ScriptSources.Add(new ScriptInfo());
-            var t = ScriptSources.GetAll();
-            ScriptSources.GetAll().Count().Should().Be(expectedCount);
-            ScriptSources.RemoveAll();
+                for (int i = 0; i < forI; i++)
+                    ScriptSources.Add(new ScriptInfo());
+                ScriptSources.GetAll().Count().Should().Be(expectedCount);
+                ScriptSources.RemoveAll();
         }
 
         [Fact]
         public void RemoveAll_ReturnsZeroAsCurrentCount()
         {
-            ScriptSources.RemoveAll();
-            ScriptSources.GetAll().Count().Should().Be(0);
+                ScriptSources.Add(new ScriptInfo());
+                ScriptSources.RemoveAll();
+                ScriptSources.GetAll().Count().Should().Be(0);
         }
 
         [Theory]
         [InlineData(0,0)]
-        [InlineData(2,2)]
         [InlineData(3,3)]
         public void AddRange_ReturnsCurrentCount(int forI, int expectedCount)
         {
-            List<ScriptInfo> list = new List<ScriptInfo>();
-            for (int i = 0; i < forI; i++)
-                list.Add(new ScriptInfo());
-            ScriptSources.AddRange(list);
-            ScriptSources.GetAll().Count().Should().Be(expectedCount);
-            ScriptSources.RemoveAll();
+                List<ScriptInfo> list = new List<ScriptInfo>();
+                for (int i = 0; i < forI; i++)
+                    list.Add(new ScriptInfo());
+                ScriptSources.AddRange(list);
+                ScriptSources.GetAll().Count().Should().Be(expectedCount);
+                ScriptSources.RemoveAll();
         }
     }
 }
